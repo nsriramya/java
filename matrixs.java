889adad9c5bd8e248e1eb5a8a331aeabd4d1b7ea
NALLAMSETTY SRI RAMYA <71599848+nsriramya@users.noreply.github.com>
2022-04-14 15:03:41 +0530

    Create matrixs.java

---
+++ matrixs.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class srthh {
+
+	public static void main(String[] args) {
+		int  c,r;
+		 int s1=0,s2=0;
+		 
+		Scanner sc=new Scanner(System.in);
+		r=sc.nextInt();
+		c=sc.nextInt();
+		int a[][];
+		a=new int[r][c];
+ 
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+		{
+			a[i][j]=sc.nextInt();
+		}
+	}
+		 
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+		{
+			if(i==j)
+			{
+			s1=s1+a[i][j];	
+			}
+		}
+	}
+	 
+for(int i=0;i<r;i++)
+{
+	for(int j=0;j<c;j++)
+	{
+		if(i+j==(r-1))
+		{
+		s2=s2+a[i][j];	
+		}
+	}
+}
+System.out.println(Math.abs(s1-s2));
+ 
+	}
+}
+	
