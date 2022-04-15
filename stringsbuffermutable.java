b6c515d59a08d6dd91de8f4195a0ce3bd8870726
NALLAMSETTY SRI RAMYA <71599848+nsriramya@users.noreply.github.com>
2022-04-15 10:29:34 +0530

    Create stringsbuffermutable.java
    string is immutable in java but "s1=" means it creates another memmory in heap
    stringBuffer is  mutable in java

---
+++ stringsbuffermutable.java
@@ -0,0 +1,20 @@
+ import java.util.*;
+//import java.util.Scanner;
+
+public class srthh {
+
+	public static void main(String[] args) {
+		 String s1=new String("aditya");
+		  s1=s1.concat("eng");
+ 
+			System.out.println(s1);
+			 StringBuffer s11=new StringBuffer("aditya");
+			  s11.append("eng");
+	 
+				System.out.println(s11);
+		}
+	
+	}
+
+			
+ 	
